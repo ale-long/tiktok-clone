88c264ac72a6b05b2dbbe5fe2571a0583e2f7986
Alex <alelong@ucdavis.edu>
2021-03-24 18:37:27 -0700

    added sidebar icons

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./VideoSidebar.css"
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
+
 function VideoSidebar() {
     return (
         <div className="videoSidebar">
-            
+            <div className="videoSidebar__button">
+                <FavoriteIcon fontSize="large"/>
+                <p>100</p>
+            </div>
+            <div className="videoSidebar__button">
+                <MessageIcon fontSize="large"/>
+                <p>50</p>
+            </div>
+            <div className="videoSidebar__button">
+                <ShareIcon fontSize="large"/>
+                <p>20</p>
+            </div>
         </div>
     )
 }
